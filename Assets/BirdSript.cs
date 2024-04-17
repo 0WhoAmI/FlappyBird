0c05b0fe582b9e620ddeec0c0f48ce7a6a010191
Bartek <bartlomiej.krzysztof.studia@gmail.com>
2024-04-17 14:56:17 +0200

    Create project

---
+++ Assets/BirdSript.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class BirdSript : MonoBehaviour
+{
+    public Rigidbody2D myRigidbody;
+    public float flapStrength;
+    public LogicScript logic;
+    public bool birdIsAlive = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
+        {
+            myRigidbody.velocity = Vector2.up * flapStrength;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
+    }
+}
